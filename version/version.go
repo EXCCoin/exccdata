376066ab05d9223528c368319119116675cda91a
harzo <harzojunior@gmail.com>
2018-10-17 11:32:08 +0200

    Update version

--- version/version.go
+++ version/version.go
@@ -35,12 +35,12 @@ var (
 	// appPreRelease is defined as a variable so it can be overridden during the
 	// build process. It MUST only contain characters from semanticAlphabet per
 	// the semantic versioning spec.
-	appPreRelease = "pre"
+	appPreRelease = ""
 
 	// appBuild is defined as a variable so it can be overridden during the
 	// build process. It MUST only contain characters from semanticBuildAlphabet
 	// per the semantic versioning spec.
-	appBuild = "dev"
+	appBuild = ""
 )
 
 // Version returns the application version as a properly formed string per the
